6d32215d1212b19b10c465600ebca2a069588b4b
Harsh Bhardwaj <hbhardwaj@ptc.cm>
2017-09-15 18:01:33 +0530

    request to TWX analytics server is called

--- index.js
+++ index.js
@@ -19,32 +19,58 @@ app.get("/set",function (req,res) {
     console.log("Aggregation api call");
     res.send("Aggregation Api flow");
 
-    var options = {
+
+
+
+
+    //for callback first
+    var options1 = {
         headers: {'Authorization':'Basic M2I0YmY3MjliMjYwMjA6YmM1NjdkMzBiNDc3OTc='},
         url: 'https://sandbox.watershedlrs.com/api/organizations/4521/aggregation/csv?config={"filter":{"groupTypeNames": null ,"activityIds":null,"personIds":null,"groupIds":null,"dateFilter":null},"dimensions":[{"type":"STATEMENT_PROPERTY","statementProperty":"result.durationCentiseconds"},{"type":"STATEMENT_PROPERTY","statementProperty":"actor.person.email"},{"type":"STATEMENT_PROPERTY","statementProperty":"context.contextActivities.parent.id"},{"type":"STATEMENT_PROPERTY","statementProperty":"actor.person.id"},{"type":"STATEMENT_PROPERTY","statementProperty":"actor.person.name"},{"type":"TIME","timePeriod":"CUSTOM","format": "ha"}],"measures":[{"name":"Interaction Count","valueProducer":{"statementProperty":"id","type":"STATEMENT_PROPERTY"},"aggregation":{"type":"COUNT"}}]}'
 
 };
-    request(options, callback);
+    request(options1, callback1);
 
-    function callback(error, response, body) {
+    function callback1(error, response, body) {
         if (error) {
-            console.log("NO error");
+            console.log("error with first call back");
         } else {
             //console.log(body);
             var lines = body.split('\n')
             lines.splice(1,1);
             var newcsv=lines.join('\n');
-            csv=newcsv;
-            console.log(newcsv);
+
         }
-    }
-console.log(csv);
+        if(newcsv!=null){
 
+            console.log("service 1 works good");
+            console.log(newcsv);
 
+        var options2;
+        options2 = {
+            headers: {
+                'Accept': 'application/json',
+                'neuron-application': '63aa7e3d040c3aa3eca58680ff5330a661628f2a',
+                'neuron-application-key': '9'
+            },
+            url: 'http://34.233.93.143/1.0/datasets/PLMS_datsets',
+            body: 'newcsv'
+        };
+
+        request(options2,callback2);
+            function callback2(error, response, body) {
+                if (error) {
+                    console.log("error with second call back");
+                } else {
+                    console.log("response of second callback" +response);
+                }
+            }
+
+    }}
 
 })
 //deployment on server
-var port=process.env.PORT || 3007;
+var port=process.env.PORT || 3006;
 app.listen(port,function (req,res) {
     console.log("server started");
 
