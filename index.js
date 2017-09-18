3d8e35b5c5f7644ba942b7939b30cba79c8cc0f4
Harsh Bhardwaj <hbhardwaj@ptc.cm>
2017-09-18 11:19:21 +0530

    all the api is added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express=require('express');
 var fs = require('fs');
 var getLine = require('get-line');
 var bodyParser = require("body-parser");
+
 var request = require('request');
 var app=express();
 
@@ -13,7 +14,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 // // public middleware for front-end
 app.use(express.static("public"));
 
-var csv;
+var newcsv;
+global.newcsv;
+
   //collecting Agg.api data
 app.get("/set",function (req,res) {
     console.log("Aggregation api call");
@@ -32,45 +35,88 @@ app.get("/set",function (req,res) {
     request(options1, callback1);
 
     function callback1(error, response, body) {
-        if (error) {
-            console.log("error with first call back");
-        } else {
+        if (!error) {
             //console.log(body);
             var lines = body.split('\n')
-            lines.splice(1,1);
-            var newcsv=lines.join('\n');
-
-        }
-        if(newcsv!=null){
-
-            console.log("service 1 works good");
+            lines.splice(1, 1);
+             newcsv = lines.join('\n');
             console.log(newcsv);
 
-        var options2;
-        options2 = {
-            headers: {
-                'Accept': 'application/json',
-                'neuron-application': '63aa7e3d040c3aa3eca58680ff5330a661628f2a',
-                'neuron-application-key': '9'
-            },
-            url: 'http://34.233.93.143/1.0/datasets/PLMS_datsets',
-            body: 'newcsv'
-        };
+        } else {
+            console.log("error with first call back");
+        }
 
-        request(options2,callback2);
-            function callback2(error, response, body) {
-                if (error) {
-                    console.log("error with second call back");
-                } else {
-                    console.log("response of second callback" +response);
-                }
-            }
+    }
 
-    }}
 
-})
+
+/*
+    var options2;
+    options2 = {
+        headers: {
+            'Content-Type':'application/json',
+            'Accept': 'application/json',
+            'neuron-application': '63aa7e3d040c3aa3eca58680ff5330a661628f2a',
+            'neuron-application-key': '9'
+        },
+        url: 'http://34.233.93.143/1.0/datasets/'
+        //body: 'newcsv'
+    };
+    request(options2,callback2);
+    function callback2(error, response, body) {
+        if (error) {
+            console.log("error with second call back");
+        } else {
+            console.log("response of second callback " + response);
+        }
+
+    }
+  */
+
+});
+
+app.get("/post",function (req,res) {
+    console.log("POST data to analytics Engine");
+    res.send("posting data to analytics engine");
+
+    request.post({
+        headers: {
+            'Content-Type':'application/json',
+            'Accept': 'application/json',
+            'neuron-application': '63aa7e3d040c3aa3eca58680ff5330a661628f2a',
+            'neuron-application-key': '9'
+        },
+        url: 'http://34.233.93.143/1.0/datasets/',
+        body: {'file':newcsv}
+    }, function(error, response, body){
+        console.log(body);
+    });
+
+
+});
+
+app.get("/version",function (req,res) {
+    console.log("checking for version...");
+
+    request.get({
+        headers: {
+            'Content-Type':'application/json',
+            'Accept': 'application/json',
+            'neuron-application': '63aa7e3d040c3aa3eca58680ff5330a661628f2a',
+            'neuron-application-key': '9'
+        },
+        url: 'http://34.233.93.143/1.0/about/versioninfo',
+    }, function(error, response, body){
+        console.log(body);
+    });
+
+
+});
+
+
+
 //deployment on server
-var port=process.env.PORT || 3006;
+var port=process.env.PORT || 3007;
 app.listen(port,function (req,res) {
     console.log("server started");
 
