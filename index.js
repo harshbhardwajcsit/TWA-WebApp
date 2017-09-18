49588733128c0ca860dd0b235d8ae0da5f554981
Harsh Bhardwaj <hbhardwaj@ptc.cm>
2017-09-18 13:05:24 +0530

    all the api is added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 //libraries include
 var express=require('express');
 var fs = require('fs');
+var FormData = require('form-data');
 var getLine = require('get-line');
 var bodyParser = require("body-parser");
 
@@ -49,47 +50,27 @@ app.get("/set",function (req,res) {
     }
 
 
-
-/*
-    var options2;
-    options2 = {
-        headers: {
-            'Content-Type':'application/json',
-            'Accept': 'application/json',
-            'neuron-application': '63aa7e3d040c3aa3eca58680ff5330a661628f2a',
-            'neuron-application-key': '9'
-        },
-        url: 'http://34.233.93.143/1.0/datasets/'
-        //body: 'newcsv'
-    };
-    request(options2,callback2);
-    function callback2(error, response, body) {
-        if (error) {
-            console.log("error with second call back");
-        } else {
-            console.log("response of second callback " + response);
-        }
-
-    }
-  */
-
 });
 
-app.get("/post",function (req,res) {
+
+
+
+
+
+app.post("/post",function (req,res) {
     console.log("POST data to analytics Engine");
     res.send("posting data to analytics engine");
 
     request.post({
         headers: {
-            'Content-Type':'application/json',
             'Accept': 'application/json',
             'neuron-application': '63aa7e3d040c3aa3eca58680ff5330a661628f2a',
             'neuron-application-key': '9'
         },
-        url: 'http://34.233.93.143/1.0/datasets/',
+        url: 'http://34.233.93.143/1.0/datasets/PLMS_d/data',
         body: {'file':newcsv}
     }, function(error, response, body){
-        console.log(body);
+        //console.log(body);
     });
 
 
@@ -108,15 +89,76 @@ app.get("/version",function (req,res) {
         url: 'http://34.233.93.143/1.0/about/versioninfo',
     }, function(error, response, body){
         console.log(body);
+        res.send(body)
     });
+
+});
+
+app.get("/data",function (req,res) {
+    console.log("Existing datasets...");
+
+    request.get({
+        headers: {
+            'Content-Type':'application/json',
+            'Accept': 'application/json',
+            'neuron-application-id': '63aa7e3d040c3aa3eca58680ff5330a661628f2a',
+            'neuron-application-key': '9'
+        },
+        url: 'http://34.233.93.143/1.0/datasets',
+    }, function(error, response, body){
+        console.log(body);
+        res.send(body);
+
+    });
+
+
+    var form = new FormData();
+    form.append('file',newcsv);
+    request.post({
+        headers: {
+            'Accept': 'application/json',
+            'neuron-application-id': '63aa7e3d040c3aa3eca58680ff5330a661628f2a',
+            'neuron-application-key': '9'
+        },
+        body:form,
+        url: 'http://34.233.93.143/1.0/datasets/PLMS_d/data',
+    }, function(error, response, body){
+        console.log(body);
+        res.send(body);
+
+    });
+
+
 
 
 });
 
 
 
+app.get("/upload",function (req,res) {
+    console.log("uploading dataset...");
+    var form = new FormData();
+    form.append('file',newcsv);
+    request.post({
+        headers: {
+            'Accept': 'application/json',
+            'neuron-application-id': '63aa7e3d040c3aa3eca58680ff5330a661628f2a',
+            'neuron-application-key': '9'
+        },
+        body:form,
+        url: 'http://34.233.93.143/1.0/datasets/PLMS_d/data',
+    }, function(error, response, body){
+        console.log(body);
+        res.send(body);
+
+    });
+
+});
+
+
+
 //deployment on server
-var port=process.env.PORT || 3007;
+var port=process.env.PORT || 3005;
 app.listen(port,function (req,res) {
     console.log("server started");
 
